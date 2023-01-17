a3fb10cb522c9a7248db7141735e757518465c64
“Matthew <meleuteri2@gmail.com>
2023-01-17 12:46:06 -0500

    nothing much hbu?

--- image-gen.py
+++ image-gen.py
@@ -1,16 +1,18 @@
 import os
 import openai
-from dotenv import load_dotenv
+import dotenv
+import webbrowser
 
-load_dotenv()
+dotenv.load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.Image.create(
-  prompt="An oil painting of the Massachusetts state house in the reain",
-  n=2,
+  prompt="A close up of pepperoni pizza under a bright light",
+  n=1,
   size="1024x1024"
 )
 
 image_url = response['data'][0]['url']
 
 print(image_url)
+webbrowser.open_new_tab(image_url)
