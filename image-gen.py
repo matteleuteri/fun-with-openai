74210567bed7dfa922d785f72e1c345c59524e67
“Matthew <meleuteri2@gmail.com>
2023-02-11 12:58:52 -0500

    Having more fun

--- image-gen.py
+++ image-gen.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.Image.create(
-  prompt="A close up of pepperoni pizza under a bright light",
+  prompt="A stained glass style painting of the last supper",
   n=1,
   size="1024x1024"
 )
